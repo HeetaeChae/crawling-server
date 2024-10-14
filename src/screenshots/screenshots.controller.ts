871034af6c1b7d7b14afecf75c7599f47d733091
chaeheetae <chaeheetae94@gmail.com>
2024-10-14 23:20:18 +0900

    비디오 이미지 가져오기, 비디오 이미지 삭제하기 구현

--- src/screenshots/screenshots.controller.ts
+++ src/screenshots/screenshots.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Get, Post, Query } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Query, Res } from '@nestjs/common';
 import { ScreenshotsService } from './screenshots.service';
+import { Response } from 'express';
 
 @Controller('api/screenshots')
 export class ScreenshotsController {
   constructor(private readonly screenshotsService: ScreenshotsService) {}
 
-  @Get('/video')
+  @Get('video/capture')
   async captureVideo(
     @Query('videoUrl') videoUrl: string,
     @Query('count') count: string,
+    @Query('title') title: string,
   ) {
     const numberedCount = Number(count);
-    return this.screenshotsService.captureVideo(videoUrl, numberedCount);
+    return this.screenshotsService.captureVideo(videoUrl, numberedCount, title);
+  }
+
+  @Get('video/:imageName')
+  async getImage(@Param('imageName') imageName: string, @Res() res: Response) {
+    const imagePath = await this.screenshotsService.getImage(imageName);
+    return res.sendFile(imagePath);
+  }
+
+  @Delete('video/:imageName')
+  async deleteImage(@Param('imageName') imageName: string) {
+    return this.screenshotsService.deleteImage(imageName);
   }
 }
--- src/screenshots/screenshots.service.ts
+++ src/screenshots/screenshots.service.ts
@@ -1,57 +1,74 @@
-import { Injectable } from '@nestjs/common';
-import { resolve } from 'path';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { unlink } from 'fs';
+import { join, resolve } from 'path';
 import * as puppeteer from 'puppeteer';
 
 @Injectable()
 export class ScreenshotsService {
-  async captureVideo(videoUrl: string, count: number) {
+  // 프로젝트 루트 디렉토리의 image 폴더 경로 설정
+  private readonly imageDir = resolve('image');
+
+  // 비디오 캡쳐하기
+  async captureVideo(videoUrl: string, count: number, title: string) {
     // 브라우저 열기
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       defaultViewport: null,
     });
+
+    // 페이지 생성
     const page = await browser.newPage();
 
-    // 비디오 웹페이지 url로 이동
-    await page.goto(videoUrl);
+    // 비디오 웹페이지 url로 이동 및 열릴 때 까지 기다림
+    await page.goto(videoUrl, { waitUntil: 'networkidle0' });
+
+    // 비디오 스크린샷 뷰포트 세팅
+    await page.setViewport({ width: 1280, height: 720 });
 
     // 비디오 간격
     const videoDuration = await page.$eval('video', (el) => el.duration);
     const interval = videoDuration / count;
-
-    // 비디오 플레이
-    await page.evaluate(() => {
-      document.querySelector('video')?.play();
-    });
 
     // 비디오 전체화면으로 전환
     await page.evaluate(() => {
       document.querySelector('video')?.requestFullscreen();
     });
 
-    for (let i = 0; i < 5; i += 1) {
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(page.screenshot({ path: 'screenshot.png' }));
-        }, 1000);
+    // 비디오 플레이
+    await page.evaluate(() => {
+      document.querySelector('video')?.play();
+    });
+
+    // 스크린샷 캡쳐
+    for (let i = 1; i <= count; i += 1) {
+      const delay = i === 1 ? 0 : interval * 1000;
+      await new Promise((res) => {
+        // 스크린샷 경로 지정
+        const imagePath = resolve(this.imageDir, `${title}_${i}.png`);
+        return setTimeout(() => {
+          res(page.screenshot({ path: imagePath }));
+        }, delay);
       });
     }
 
-    /*
-    // 비디오 스크린샷
-    await page.screenshot({ path: 'screenshot.png' });
-    */
+    await browser.close();
+  }
 
-    /*
-    const screenshots: string[] = [];
-    const screenshotsPath = `screenshot.png`;
-    await page.screenshot({ path: screenshotsPath });
-    */
+  // 이미지 가져오기
+  async getImage(imageName: string) {
+    const imagePath = join(process.cwd(), 'image', imageName);
+    return imagePath;
+  }
 
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(browser.close());
-      }, 1000 * 10);
+  // 이미지 삭제하기
+  async deleteImage(imageName: string) {
+    const imagePath = await this.getImage(imageName);
+    unlink(imagePath, (err) => {
+      if (err) {
+        throw new InternalServerErrorException('이미지 삭제 실패');
+      } else {
+        return `이미지 (${imageName}) 삭제 성공`;
+      }
     });
   }
 }
